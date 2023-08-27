a6c4af8a3cbeb860912ff4382f65e74e8cfc441c
GauravSighariya <84697605+GauravSighariya@users.noreply.github.com>
2023-08-27 23:32:32 +0530

    Update models.py

--- audio_app/models.py
+++ audio_app/models.py
@@ -6,7 +6,7 @@ class UploadedAudio(models.Model):
     upload_date = models.DateTimeField(auto_now_add=True)
     file_size = models.PositiveIntegerField(blank=True,null=True)
     duration = models.FloatField(blank=True,null=True)  # Duration of the audio in seconds
-    # extension = models.CharField(max_length=10,blank=True,null=True)
+    extension = models.CharField(max_length=10,blank=True,null=True)
 
     def __str__(self):
         return self.audio_file.name
